c8ba0f3ffa98471a6100b54a9a373f09004cbe24
Brendon Clarke <w0421167@campus.nscc.ca>
2019-03-26 13:36:43 -0300

    Like

--- src/routes/chat.js
+++ src/routes/chat.js
@@ -9,7 +9,7 @@ router.get('/chat', async function getMessages(req, res, next) {
 
   // TODO: create like model
   // TODO: get like count from like model
-  const likeCount = 1
+  const likeCount = 0
   // TODO: get whether signed in user likes this from model
   const iLikeThis = true
 
